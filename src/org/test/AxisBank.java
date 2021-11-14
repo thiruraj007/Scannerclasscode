86e0b126cabe8db20c1e9121ec139148fa819df0
thiruraj007 <thiru.mech75@gmail.com>
2021-11-14 09:50:00 +0530

    scannerclass

---
+++ src/org/test/AxisBank.java
@@ -0,0 +1,27 @@
+package org.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AxisBank extends IdfcBank {
+
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("D:\\cv");
+		String[] list = file.list();
+		for (String x : list) {
+			System.out.println(x);
+		}
+		System.out.println("******************************************************");
+		File[] listFiles = file.listFiles();
+		for (File y : listFiles) {
+			
+			System.out.println(y);
+		}
+		
+		
+		
+		
+
+}
+}
---
+++ src/org/test/IdfcBank.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class IdfcBank {
+
+	public void Deposit() {
+		// TODO Auto-generated method stub
+System.out.println("Idfc savings is :13%");
+	}
+}
